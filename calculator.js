b9cc81d30ddab73a53d0f1adc7188315f1e315e8
Luis Alvarez <johnlavz34@gmail.com>
2025-07-18 01:34:20 -0500

    functionality to add active class to porcentage buttons  section

--- calculator.js
+++ calculator.js
@@ -5,13 +5,24 @@ const amount = document.getElementById("input-bill");
 const tipPorcentage = document.querySelectorAll(".porcentage-selected");
 const tipAmount = document.getElementById("tip-amount");
 const totalAmount = document.getElementById("total-cost");
+const porcentage = document.querySelector(".porcentage");
+
+amount.addEventListener("input", () => {
+  if (amount.value.trim() !== "") {
+    porcentage.classList.add("active");
+  }
+});
 
 //functionality to select tip porcentage
 tipPorcentage.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const selected = parseFloat(btn.dataset.five);
-    const bill = parseFloat(amount.value);
-    const tip = bill * (selected / 100);
-    tipAmount.value = "$" + tip.toFixed(2);
-  });
+  if (amount.value.trim() !== "") {
+    btn.addEventListener("click", () => {
+      const selected = parseFloat(btn.dataset.five);
+      const bill = parseFloat(amount.value);
+      const tip = bill * (selected / 100);
+      tipAmount.value = "$" + tip.toFixed(2);
+    });
+  } else {
+    amount.placeholder = "Please enter the bill amount";
+  }
 });
