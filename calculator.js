bb36c840f8ca7860b1c29a8eb33b57829d76e366
Luis Alvarez <johnlavz34@gmail.com>
2025-07-17 23:23:05 -0500

    Adding functionalty for porcentage tip selection

--- calculator.js
+++ calculator.js
@@ -4,4 +4,15 @@
 const amount = document.getElementById("input-bill");
 const tipPorcentage = document.querySelectorAll(".porcentage-selected");
 const tipAmount = document.getElementById("tip-amount");
-const totalAmount = document.getElementById("total-amount");
+const totalAmount = document.getElementById("total-cost");
+
+let selected = 0;
+let bill = 0;
+
+tipPorcentage.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    selected = parseFloat(btn.dataset.five);
+    bill = parseFloat(amount.value);
+    tipAmount.value = bill * (selected / 100);
+  });
+});
