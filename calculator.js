c0e2d34467ad1820be982d38030300b31e157239
Luis Alvarez <johnlavz34@gmail.com>
2025-07-18 19:54:40 -0500

    Adding functionality to activate only one porcentage button at a time

--- calculator.js
+++ calculator.js
@@ -17,6 +17,9 @@ amount.addEventListener("input", () => {
 tipPorcentage.forEach((btn) => {
   btn.addEventListener("click", () => {
     calculate.classList.add("active");
+    tipPorcentage.forEach((b) => {
+      b.classList.remove("selected");
+    });
     btn.classList.add("selected");
     selected = parseFloat(btn.dataset.tip);
   });
